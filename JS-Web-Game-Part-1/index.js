c29dca9f96a35f2496a120cc603a02e0179209e1
YaheyaKG <100176826+YaheyaKG@users.noreply.github.com>
2022-05-09 19:17:45 -0400

    Add files via upload

---
+++ JS-Web-Game-Part-1/index.js
@@ -0,0 +1,33 @@
+function newImage(url, left, bottom)
+{
+let object = document.createElement('img')
+object.src = url
+object.style.position = 'fixed'
+object.style.left = left + 'px'
+object.style.bottom = bottom + 'px'
+document.body.append(object)
+return object
+}
+
+
+newImage('assets/green-Character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+function newItem(url, left, bottom)
+{
+let object = newImage(url, left, bottom)
+
+
+object.addEventListener('dblclick', function(){
+    object.remove()
+})
+return object
+}
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png',165,185)
+newItem('assets/staff.png', 600, 100)
+
